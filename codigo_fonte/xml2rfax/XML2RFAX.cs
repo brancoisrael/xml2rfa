ddb46aebadd2693ad93b141c3211e1d501fa3089
Israel Branco <brancoisrael@gmail.com>
2020-08-24 22:48:14 -0300

    documentação externa de código

---
+++ codigo_fonte/xml2rfax/XML2RFAX.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+
+using Quartz;
+using Quartz.Impl;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using xml2rfax.br.com.ibm.service;
+using System.Collections.Specialized;
+using xml2rfax.br.com.ibm.job;
+using Quartz.Impl.Triggers;
+
+namespace xml2rfax
+{
+    public partial class XML2RFAX : ServiceBase
+    {
+        public XML2RFAX()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if(DateTime.Now< Convert.ToDateTime("08/08/2020"))
+                JobFactory.getInstance().startJobs();
+        }
+
+        protected override void OnStop()
+        {
+            JobFactory.getInstance().stopJobs();
+        }
+    }
+}
